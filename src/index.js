2364e491777de945d2ecb8791ea7d109c0e27c1a
darkunchain <luisfer74081@gmail.com>
2019-02-25 18:40:58 -0500

    ejs

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const raiz = __dirname;
-
+//const cons = require('consolidate');
 
 //settings
 app.set('port', 3000);
 app.set('views', path.join(raiz,'views'));
+//app.engine('html', cons.swig);
+//app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
