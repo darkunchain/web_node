3bc0ff8136eb31cfec43ca579bb48d463358e687
darkunchain <luisfer74081@gmail.com>
2019-02-26 19:51:37 -0500

    estilos

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,28 +14,28 @@ router.get('/about', (req, res) => {
   res.render('about.html', { title: 'Acerca de nosotros' })
 });
 
-router.get('/productos', (req,res) => {
-  res.render('prod.html',{title:'Productos y servicios'})
+router.get('/productos', (req, res) => {
+  res.render('prod.html', { title: 'Productos y servicios' })
 });
 
-router.get('/apps', (req,res) => {
-  res.render('apps.html',{title:'Aplicaciones'})
+router.get('/apps', (req, res) => {
+  res.render('apps.html', { title: 'Aplicaciones' })
 });
 
-router.get('/catalogo', (req,res) => {
-  res.render('catalogo.html',{title:'Catalogo'})
+router.get('/catalogo', (req, res) => {
+  res.render('catalogo.html', { title: 'Catalogo' })
 });
 
-router.get('/empleados', (req,res) => {
-  res.render('empleados.html',{title:'Empleados'})
+router.get('/empleados', (req, res) => {
+  res.render('empleados.html', { title: 'Empleados' })
 });
 
-router.get('/menu5', (req,res) => {
-  res.render('menu5.html',{title:'Menu5'})
+router.get('/menu5', (req, res) => {
+  res.render('menu5.html', { title: 'Menu5' })
 });
 
-router.get('/blog', (req,res) => {
-  res.render('blog.html',{title:'Blog'})
+router.get('/blog', (req, res) => {
+  res.render('blog.html', { title: 'Blog' })
 });
 
 
